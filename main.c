91ab91e07b36d929af2ff7496f0f8c95f20c6340
mxttywxtty <mathijscusters@gmail.com>
2025-11-02 11:01:53 +0100

    feat: better argument parsing using argp

--- main.c
+++ main.c
@@ -2,30 +2,70 @@
 #include "include/board.h"
 #include "include/game.h"
 #include "include/assets.h"
+#include "include/save.h"
+#include <argp.h>
+
+// https://stackoverflow.com/a/24479532
+const char *argp_program_version = "Minesweeper 1.0";
+const char *argp_program_bug_address = "mathijs.custers@vub.be";
+
+static const char doc[] = "A simple minesweeper implementation";
+static const char args_doc[] = "";
+
+static struct argp_option options[] = {
+    { "rows", 'w', "ROWS", 0, "The amount of rows of the playing board" },
+    { "cols", 'h', "COLS", 0, "The amount of columns of the playing board" },
+    { "mines", 'm', "MINES", 0, "The amount of mines the playing board has" },
+    { "file", 'f', "FILE", 0, "The file from which to load an existing game" },
+    { 0 }
+};
+
+struct arguments {
+    int rows;
+    int cols;
+    int mines;
+    char *file;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *arguments = state->input;
+    switch (key) {
+        char *endptr;
+        case 'w': arguments->rows = strtol(arg, &endptr, 10); break;
+        case 'h': arguments->cols = strtol(arg, &endptr, 10); break;
+        case 'm': arguments->mines = strtol(arg, &endptr, 10); break;
+        case 'f': arguments->file = arg; break;
+        default: return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
 
 int main(int argc, char *argv[]) {
+    struct arguments arguments = { .rows = 9, .cols = 9, .mines = 9 };
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    srand((unsigned) time(NULL)); // Initialize a random seed based on int time value
+
     GameState gs;
     InputState input;
     Assets assets;
 
-    int rows = 9;
-    int cols = 9;
-    int mines = 9;
-
-    if (argc >= 4){
-        rows = atoi(argv[1]);
-        cols = atoi(argv[2]);
-        mines = atoi(argv[3]);
-    } else {
-        printf("No custom dimensions provided, resuming with default config: (%dx%d) grid, %d mines\n", rows, cols, mines);
-    }
-
-    init_gui(&gs, &input, GAME_NAME, cols * CELL_WIDTH, rows * CELL_HEIGHT);
+    init_gui(&gs, &input, GAME_NAME, arguments.cols * CELL_WIDTH, arguments.rows * CELL_HEIGHT);
     load_assets(gs.renderer, &assets);
 
-    srand((unsigned) time(NULL)); // Initialize a random seed based on int time value
+    Board *board;
+    if (arguments.file) {
+        printf("Loading existing game from load file: %s\n", arguments.file);
+        board = load_game(arguments.file);
+    } else {
+        printf("Started a new game: (%dx%d), %d mines\n", arguments.rows, arguments.cols, arguments.mines);
+        board = new_game(&gs, arguments.rows, arguments.cols, arguments.mines);
+    }
 
-    Board *board = new_game(&gs, rows, cols, mines);
     while(gs.should_continue) {
         read_input(&gs, &input);
         update_game(&gs, board, &input);
