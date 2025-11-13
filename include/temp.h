67f19512493cd6a2db0ce6a5aa24bc20e55bd661
mxttywxtty <mathijscusters@gmail.com>
2025-11-13 19:16:54 +0100

    chore: create `temp.h` files to represent animations

---
+++ include/temp.h
@@ -0,0 +1,19 @@
+#ifndef TEMP_H_
+#define TEMP_H_
+
+#include "board.h"
+
+// Temporary structs needed to animate wins and losses
+typedef struct {
+    Cell *cell;
+    int count;
+    int done;
+} TempLoss;
+
+typedef struct {
+    Cell **cells;
+    int amount;
+    int count;
+    int done;
+} TempWin;
+#endif
